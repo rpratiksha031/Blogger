33ee1c8138e83d8d3809b72dc1117ac16887c225
pratiksharawat <rpratiksha031@gmail.com>
2025-08-31 11:35:29 +0530

    ecosystem file added

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: "Blogger",
+      script: "node_modules/next/dist/bin/next",
+      args: "start -p 3000",
+      instances: "max",
+      exec_mode: "cluster",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+      error_file: "./logs/err.log",
+      out_file: "./logs/out.log",
+      log_file: "./logs/combined.log",
+      time: true,
+    },
+  ],
+};
